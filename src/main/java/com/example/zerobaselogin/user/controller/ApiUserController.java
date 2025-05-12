a43e2cb1bbbce03b4a7b70a586b340be5ed12b8c
[CLOUD4] 한현 <gusgus1467@naver.com>
2025-05-12 16:00:30 +0900

    feat: 사용자 정보 저장 API

--- src/main/java/com/example/zerobaselogin/user/controller/ApiUserController.java
+++ src/main/java/com/example/zerobaselogin/user/controller/ApiUserController.java
@@ -1,8 +1,11 @@
 package com.example.zerobaselogin.user.controller;
 
+import com.example.zerobaselogin.user.entity.User;
 import com.example.zerobaselogin.user.model.ResponseError;
 import com.example.zerobaselogin.user.model.UserInput;
+import com.example.zerobaselogin.user.repository.UserRepository;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -11,11 +14,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class ApiUserController {
+
+    private final UserRepository userRepository;
+
+    // 예외 처리
+//    @PostMapping("/api/user")
+//    public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors) {
+//
+//        List<ResponseError> responseErrorList = new ArrayList<>();
+//
+//        if (errors.hasErrors()) {
+//            errors.getAllErrors().forEach(e -> {
+//                responseErrorList.add(ResponseError.of((FieldError) e)); // 에러 목록이 쌓임
+//            });
+//            return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
+//        }
+//        return ResponseEntity.ok().build();
+//    }
 
     @PostMapping("/api/user")
     public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors) {
@@ -28,6 +50,25 @@ public class ApiUserController {
             });
             return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
         }
-        return ResponseEntity.ok().build();
+
+        try {
+            User user = User.builder()
+                    .email(userInput.getEmail())
+                    .userName(userInput.getUserName())
+                    .password(userInput.getPassword())
+                    .phone(userInput.getPhone())
+                    .regDate(LocalDateTime.now())
+                    .build();
+            userRepository.save(user);
+
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            // 예외 로그 출력
+            e.printStackTrace();
+            return new ResponseEntity<>("사용자 저장 중 오류 발생: " + e.getMessage(),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        //return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/example/zerobaselogin/user/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.zerobaselogin.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String userName;
+
+    @Column
+    private String password;
+
+    @Column
+    private String phone;
+
+    @Column
+    private LocalDateTime regDate;
+}
