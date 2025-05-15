e3bd3e7fa16f372afd6dd24bbf8d414acf518cb9
[CLOUD4] 한현 <gusgus1467@naver.com>
2025-05-15 15:33:31 +0900

    refac: ResponseMessage 사용

--- src/main/java/com/example/zerobaselogin/common/model/ResponseResult.java
+++ src/main/java/com/example/zerobaselogin/common/model/ResponseResult.java
@@ -1,15 +1,18 @@
 package com.example.zerobaselogin.common.model;
 
 import com.example.zerobaselogin.board.model.ServiceResult;
+import com.example.zerobaselogin.user.model.ResponseMessage;
 import org.springframework.http.ResponseEntity;
 
 public class ResponseResult {
     public static ResponseEntity<?> fail(String message) {
-        return ResponseEntity.badRequest().body(message);
+
+        return ResponseEntity.badRequest().body(ResponseMessage.fail(message));
     }
 
     public static ResponseEntity<?> succeess() {
-        return ResponseEntity.ok().build();
+
+        return ResponseEntity.ok().body(ResponseMessage.success());
     }
 
     public static ResponseEntity<?> result(ServiceResult result) {
