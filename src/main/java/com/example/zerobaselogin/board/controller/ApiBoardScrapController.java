e1153f374a21cac9fe1c03d08f57469878b5e476
[CLOUD4] 한현 <gusgus1467@naver.com>
2025-05-15 17:22:34 +0900

    feat: 게시글 스크랩 삭제 API

--- src/main/java/com/example/zerobaselogin/board/controller/ApiBoardScrapController.java
+++ src/main/java/com/example/zerobaselogin/board/controller/ApiBoardScrapController.java
@@ -13,13 +13,13 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/board")
+@RequestMapping("/api")
 public class ApiBoardScrapController {
 
     private final BoardService boardService;
 
     // 게시글 스크랩 API
-    @PutMapping("/{id}/scrap")
+    @PutMapping("/board/{id}/scrap")
     public ResponseEntity<?> boardScrap(@PathVariable("id") Long id,
                            @RequestHeader("F-TOKEN") String token) {
 
@@ -30,7 +30,21 @@ public class ApiBoardScrapController {
             return ResponseResult.fail("토큰 정보가 정확하지 않습니다.");
         }
 
-        return ResponseResult.result(boardService.scrapBoard(id, email));
+        return ResponseResult.result(boardService.scrap(id, email));
 
+    }
+
+    // 게시글의 스크랩을 삭제하는 API
+    @DeleteMapping("/scrap/{id}")
+    public ResponseEntity<?> deleteBoardScrap(@PathVariable("id") Long id,
+                                 @RequestHeader("F-TOKEN") String token) {
+        String email = "";
+        try {
+            email = JWTUtils.getIssuer(token);
+        } catch (JWTVerificationException e) {
+            return ResponseResult.fail("토큰 정보가 정확하지 않습니다.");
+        }
+
+        return ResponseResult.result(boardService.removeScrap(id, email));
     }
 }
--- src/main/java/com/example/zerobaselogin/board/service/BoardService.java
+++ src/main/java/com/example/zerobaselogin/board/service/BoardService.java
@@ -42,7 +42,10 @@ public interface BoardService {
     List<BoardBadReport> badReportList();
 
     // 게시글을 스크랩함
-    ServiceResult scrapBoard(Long id, String email);
+    ServiceResult scrap(Long id, String email);
+
+    // 게시글 스크랩 삭제
+    ServiceResult removeScrap(Long id, String email);
 
     // 게시판 타입의 게시글 수를 리턴
     //List<BoardTypeCount> getBoardTypeCount();
